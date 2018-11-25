bc239b69c5a7afdb566949b6271238a6077490db
Arthur Vernet <arthurvernet@gmail.com>
2018-11-25 11:24:47 +0100

    gitignore

--- scripts/script.py
+++ scripts/script.py
@@ -3,9 +3,11 @@ from pyspark.sql import *
 spark = SparkSession.builder.getOrCreate()
 
 DATASET_PATH = "hdfs:///datasets/reddit_data"
-PATH_2007 = DATASET_PATH + "/2007/RC_2007-12.bz2"
+PATH_2005 = DATASET_PATH + "/2005/RC_2005-12.bz2"
 
-print(PATH_2007)
-reddit = spark.read.json(PATH_2007) # open the file
-reddit.show(5)
+print(PATH_2005)
+reddit = spark.read.json(PATH_2005) # open the file
+
 reddit.printSchema()
+
+reddit.write.format('json').save('/Users/arthishow/Desktop/reddit.json')
