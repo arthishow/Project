a3f70d1d3e51458fa5b6e9cc741f30d81abde09f
Arthur Vernet <arthurvernet@gmail.com>
2018-12-02 11:57:43 +0100

    fixed script

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-spark-submit --master yarn --deploy-mode client --driver-memory 4G --num-executors 5 --executor-memory 4G --executor-cores 5 $1
+spark-submit --master yarn --deploy-mode cluster --driver-memory 4G --num-executors 5 --executor-memory 4G --executor-cores 5 $1
