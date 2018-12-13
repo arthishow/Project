c5f8e217d11b4217d91f0058a02ba2eeb42bf462
Arthur Vernet <arthurvernet@gmail.com>
2018-12-13 11:58:22 +0100

    fixed script, continuation of the main notebook

--- scripts/script.py
+++ scripts/script.py
@@ -8,5 +8,5 @@ for month in range(6, 12):
     PATH = DATASET_PATH + '/2016/RC_2016-{num:02d}.bz2'.format(num=month)
     comments = spark.read.json(PATH)
     comments = comments.filter(comments['subreddit'] == 'politics').filter(comments['body'] != '[deleted]')
-    comments = comments.select('author', 'author_flair_text', 'body', 'created_utc', 'gilded', 'link_id', 'parent_id', 'score')
+    comments = comments.select('author', 'author_flair_text', 'body', 'created_utc', 'gilded', 'id', 'link_id', 'parent_id', 'score')
     comments.write.mode('overwrite').parquet('politics_2016_{num:02d}'.format(num=month))
